b0dc39b0465d2d957d2855f0331ce4ca1394e3bd
GiacomoViolini <giacvio@gmail.com>
2023-08-06 20:12:52 +0200

    initial route fix

--- App.js
+++ App.js
@@ -10,27 +10,38 @@ const Stack = createStackNavigator();
 
 export default function App() {
   const [registered, setRegistered] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkRegistrationStatus = async () => {
       try {
         const name = await AsyncStorage.getItem("name-key");
         const email = await AsyncStorage.getItem("email-key");
+        console.log(name, email);
         if (name && email) {
-          // value previously stored
           setRegistered(true);
+        } else {
+          setRegistered(false);
         }
       } catch (e) {
         // error reading value
+      } finally {
+        setLoading(false);
       }
     };
-
     checkRegistrationStatus();
   }, []);
 
+  if (loading) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={!registered? "OnBoarding": "HomeScreen"}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName={registered ? "HomeScreen" : "OnBoarding"}
+      >
         <Stack.Screen name="OnBoarding" component={OnBoarding} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="Profile" component={Profile} />
